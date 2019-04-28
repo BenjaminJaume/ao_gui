9ca1396b77c53e7695e03c5fa7c128f50f36cbae
jorus-tyron <benjamin.jaume@gmail.com>
2019-04-28 06:24:07 +0200

    Adding GPS

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 import RPi.GPIO as GPIO
 import gi, os, sys, time
-
 gi.require_version('Gtk', '3.0')
 gi.require_version('WebKit', '3.0')
 from gi.repository import Gtk, GLib, Gdk, Gio, GObject, WebKit
@@ -23,13 +23,16 @@ ledPin = 20
 ventPin = 21
 #batteryPin = UNDEFINED
 
-# setup GPIO
-#GPIO.setmode(GPIO.BCM) # Broadcom numbering system
-#GPIO.setup(ledPin, GPIO.OUT)
+# setup GPIO: BCM
+GPIO.setmode(GPIO.BCM) # Broadcom numbering system
+GPIO.setwarnings(False)
+
+# setup GPIO input / output
+GPIO.setup(ledPin, GPIO.OUT)
 #GPIO.setup(ventPin, GPIO.OUT)
 #GPIO.setup(batteryPin, GPIO.IN)
 
-# Initialise outputs
+# Initialise outputs as LOW
 #GPIO.output(ledPin, GPIO.LOW)
 #GPIO.output(ventPin, GPIO.LOW)
 
@@ -39,9 +42,11 @@ state_vent = "OFF"
 
 gpsd = None #seting the global variable
 
-temperature = 0
-pressure = 0
-humidity = 0
+global temperature
+global pressure
+global humidity
+global altitude
+global speed
 
 language = "EN"
 
@@ -57,7 +62,7 @@ class Handler:
 
         # If we have clicked the button AND the LED was switched OFF
         if state_LED == "OFF":
-            #GPIO.output(ledPin, GPIO.HIGH)
+            GPIO.output(ledPin, GPIO.HIGH)
             state_LED = "ON"
             print("LED:", state_LED)
             
@@ -66,7 +71,7 @@ class Handler:
             image.set_from_file("on.png")
             button.set_image(image)                       
         else:
-            #GPIO.output(ledPin, GPIO.LOW)
+            GPIO.output(ledPin, GPIO.LOW)
             state_LED = "OFF"
             print("LED:", state_LED)
             
@@ -107,7 +112,7 @@ class Handler:
         print("Exit application")
         gpsp.running = False
         gpsp.join() # wait for the thread to finish what it's doing
-        #GPIO.cleanup()
+        GPIO.cleanup()
 
 class GpsPoller(threading.Thread):
     def __init__(self):
@@ -164,8 +169,26 @@ class Application:
         self.startRoutine_1min()
         
     def routine_1sec(self):
-        #  Displays Timer
-        #  putting our datetime into a var and setting our label to the result. 
+        global speed
+        global altitude
+        
+        global day
+        global month
+        global year
+        global hour
+        global minute
+        global second
+        
+        global text_time
+        global text_speed
+        global text_altitude
+        
+        label_time = self.builder.get_object('label_time')
+        label_speed = self.builder.get_object('label_speed')
+        label_altitude = self.builder.get_object('label_altitude')
+        
+        # UPDATING THE TIME FROM THE OS
+        
         datetimenow = datetime.now()
         day = datetimenow.strftime("%d")
         month = datetimenow.strftime("%m")
@@ -174,34 +197,62 @@ class Application:
         minute = datetimenow.strftime("%M")
         second = datetimenow.strftime("%S")
         
-        label = self.builder.get_object('label_time')
-        label.set_label(str(day) + "/" +
-                        str(month) + "/" +
-                        str(year) + "\n" +
-                        str(hour) + ":" +
-                        str(minute) + ":" +
-                        str(second))
+        text_time = day + "/" + month + "/" + year + "\n" + hour + ":" + minute + ":" + second
+        
+        label_time.set_label(text_time)
+        
+        # RECEIVING DATA FROM GPS
+        try:        
+            #It may take a second or two to get good GPS data
 
-        try:
-            #It may take a second or two to get good data
-
-            print (' GPS reading')
-            print ('----------------------------------------')
-            print ('latitude    ' , gpsd.fix.latitude)
-            print ('longitude   ' , gpsd.fix.longitude)
-            print ('time utc    ' , gpsd.utc,' + ', gpsd.fix.time)
-            print ('altitude (m)' , gpsd.fix.altitude)
-            print ('eps         ' , gpsd.fix.eps)
-            print ('epx         ' , gpsd.fix.epx)
-            print ('epv         ' , gpsd.fix.epv)
-            print ('ept         ' , gpsd.fix.ept)
-            print ('speed (m/s) ' , gpsd.fix.speed)
-            print ('climb       ' , gpsd.fix.climb)
-            print ('track       ' , gpsd.fix.track)
-            print ('mode        ' , gpsd.fix.mode)
-            #print ('sats        ' , gpsd.satellites)
+            #print (' GPS reading')
+            #print ('----------------------------------------')
+            #print ('latitude    ' , gpsd.fix.latitude)
+            #print ('longitude   ' , gpsd.fix.longitude)
+            #print ('time utc    ' , gpsd.utc,' + ', gpsd.fix.time)
+            #print ('altitude    ' , gpsd.fix.altitude)
+            ##print ('eps         ' , gpsd.fix.eps)
+            ##print ('epx         ' , gpsd.fix.epx)
+            ##print ('epv         ' , gpsd.fix.epv)
+            ##print ('ept         ' , gpsd.fix.ept)
+            #print ('speed        ' , gpsd.fix.speed)
+            ##print ('climb       ' , gpsd.fix.climb)
+            ##print ('track       ' , gpsd.fix.track)
+            ##print ('mode        ' , gpsd.fix.mode)
+            ##print ('sats        ' , gpsd.satellites)
+            
+            #print("GPS data OK")
+            
+            # UPLOAD TIME WITH GPS
+            
+            # 2019-04-28T02:26:27.000Z
+            #time = gpsd.fix.time
+            
+            #day = time[8:10]
+            #month = time[5:7]
+            #year = time[0:4]
+            #hour = time[11:13]
+            #minute = time[14:16]
+            #second = time[17:19]
+            
+            #text_time = day + "/" + month + "/" + year + "\n" + hour + ":" + minute + ":" + second
+            
+            speed = gpsd.fix.speed
+            altitude = gpsd.fix.altitude
+            
+            text_speed = "Speed:\n" + str(round(speed)) + " km/h"
+            text_altitude = "Altitude:\n" + str(round(altitude)) + " m"
+            
         except:
             print("Problem with the GPS. Error #4: Application routine_1s")
+            
+            #label_time.set_label("--/--/----\n--:--:--")
+            
+            text_speed = "Speed:\n-- km/h"
+            text_altitude = "Altitude:\n-- m"
+        
+        label_speed.set_label(text_speed)
+        label_altitude.set_label(text_altitude)
         
         #  Return "True" to ensure the timer continues to run, otherwise it will only run once.
         return True
@@ -210,6 +261,14 @@ class Application:
         global temperature
         global pressure
         global humidity
+        
+        global text_temperature
+        global text_pressure
+        global text_humidity
+        
+        label_temperature = self.builder.get_object('label_temperature')
+        label_pressure = self.builder.get_object('label_pressure')
+        label_humidity = self.builder.get_object('label_humidity')
         
         try:
             sense = SenseHat()
@@ -223,26 +282,15 @@ class Application:
             text_pressure = "Pressure:\n" + str(int(round(pressure))) + " hPa"
             text_humidity = "Humidity:\n" + str(int(round(humidity))) + "%"
             
-            label_temperature = self.builder.get_object('label_temperature')
-            label_pressure = self.builder.get_object('label_pressure')
-            label_humidity = self.builder.get_object('label_humidity')
-            
-            label_temperature.set_label(text_temperature)
-            label_pressure.set_label(text_pressure)
-            label_humidity.set_label(text_humidity)
         except:
             print("Sense HAT and Raspberry are not communicating")
-            text_temperature = "Temperature:\n" + "--°C"
-            text_pressure = "Pressure:\n" + "---- hPa"
-            text_humidity = "Humidity:\n" + "--%"
-            
-            label_temperature = self.builder.get_object('label_temperature')
-            label_pressure = self.builder.get_object('label_pressure')
-            label_humidity = self.builder.get_object('label_humidity')
-            
-            label_temperature.set_label(text_temperature)
-            label_pressure.set_label(text_pressure)
-            label_humidity.set_label(text_humidity)
+            text_temperature = "Temperature:\n--°C"
+            text_pressure = "Pressure:\n---- hPa"
+            text_humidity = "Humidity:\n--%"
+        
+        label_temperature.set_label(text_temperature)
+        label_pressure.set_label(text_pressure)
+        label_humidity.set_label(text_humidity)
             
         #Return "True" to ensure the timer continues to run, otherwise it will only run once.
         return True
